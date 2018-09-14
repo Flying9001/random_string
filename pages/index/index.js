72e01f4547abe2861c18707b4d9d5ec7ee7289c7
junqiang.lu <flying9001@gmail.com>
2018-09-14 19:40:28 +0800

    添加强制更新功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,6 @@
 //index.js
-var common = require("../../utils/common.js");
+const common = require("../../utils/common.js");
+const updateManager = wx.getUpdateManager();
 //获取应用实例
 const app = getApp()
 
@@ -19,6 +20,24 @@ Page({
       randommessagedown: randomStrDown
     });
   },
+  /**
+   * 强制更新
+   */
+  onShow: function(){
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+  },
+
   /**
    * 获取固定位数随机字符串
    * 不包含符号
